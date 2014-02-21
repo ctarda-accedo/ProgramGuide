3aeb582162762a06bfd82921d32680808ae81f49
Cesar Tardaguila <cesar.tardaguila@accedo.tv>
2014-02-21 12:11:45 +0800

    Add project

---
+++ ProgramGuide/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ProgramGuide
+//
+//  Created by Cesar Tardaguila on 2/21/14.
+//  Copyright (c) 2014 Accedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
